a0df42cf7af4946e2181a6e293e8347498d7e5e3
Saad Niazi <saadniazifed@gmail.com>
2022-07-17 14:37:54 +0500

    Finished the CV Project using both Class based component as well as Function based components.

--- src/components/SidebarComponent/Interests/Interests.js
+++ src/components/SidebarComponent/Interests/Interests.js
@@ -1,69 +1,55 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import RenderInterests from "./RenderInterests";
 import "./Interests.css";
 
-class Interests extends Component {
-  constructor(props) {
-    super(props);
+const Interests = () => {
+  const [interestName, setInterestName] = useState("");
+  const [interestList, setInterestList] = useState([]);
 
-    this.state = {
-      interestName: { text: "" },
-      interestList: [],
-    };
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      interestName: {
-        text: e.target.value,
-      },
+  const handleChange = (e) => {
+    setInterestName({
+      name: e.target.value,
     });
   };
 
-  addInterest = (e) => {
+  const addInterest = (e) => {
     e.preventDefault();
-    this.setState({
-      ...this.interestList,
-      interestList: this.state.interestList.concat(this.state.interestName),
-      interestName: { text: "" },
-    });
+    setInterestList([
+      ...interestList,
+      {
+        interestList: [...interestList, interestName],
+      },
+    ]);
   };
 
-  removeHobby = (index) => {
-    let newArray = this.state.interestList;
-    let pos = newArray.indexOf(index);
-    newArray.splice(pos, 1);
-    this.setState({
-      interestList: newArray,
-    });
+  const removeHobby = (index) => {
+    setInterestList([
+      ...interestList.slice(0, index),
+      ...interestList.slice(index + 1, interestList.length),
+    ]);
   };
 
-  render() {
-    const { ...props } = this.state;
-
-    return (
-      <div className="interest--container">
-        <div className="form__group field interestCenter">
-          <input
-            type="text"
-            id="interestInput"
-            value={this.state.interestName.text}
-            onChange={this.handleChange}
-            className="form__field"
-            placeholder="ReactJS, JavaScript..."
-          />
-          <button
-            type="button"
-            onClick={this.addInterest}
-            className="interestButton"
-          >
-            Add Interest
-          </button>
-          <RenderInterests {...props} removeHobby={this.removeHobby} />
-        </div>
+  return (
+    <div className="interest--container">
+      <div className="form__group field interestCenter">
+        <input
+          type="text"
+          id="interestInput"
+          value={interestName.name}
+          onChange={handleChange}
+          className="form__field"
+          placeholder="ReactJS, JavaScript..."
+        />
+        <button type="button" onClick={addInterest} className="interestButton">
+          Add Interest
+        </button>
+        <RenderInterests
+          removeHobby={removeHobby}
+          interestList={interestList}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Interests;
--- src/components/SidebarComponent/Interests/RenderInterests.js
+++ src/components/SidebarComponent/Interests/RenderInterests.js
@@ -1,41 +1,31 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from "react";
+import React from "react";
 import "./Interests.css";
 import uniqid from "uniqid";
 
-class RenderInterests extends Component {
-  constructor(props) {
-    super(props);
+const RenderInterests = (props) => {
+  const { removeHobby, interestList } = props;
 
-    // this.state = {
-    //   id: 0,
-    // };
-  }
-
-  render() {
-    const { interestList, removeHobby } = this.props;
-
-    return (
-      <div className="renderInterest--container">
-        <ul className="interest-and-button interestContainer">
-          {interestList.map((task, index) => {
-            return (
-              <li key={uniqid()} className="li--container interestColumn ">
-                {task.text}
-                <button
-                  type="button"
-                  onClick={() => removeHobby(index)}
-                  className="removeHobbyBtn"
-                >
-                  Remove Hobby
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="renderInterest--container">
+      <ul className="interest-and-button interestContainer">
+        {interestList.map((task, index) => {
+          return (
+            <li key={uniqid()} className="li--container interestColumn ">
+              {task.interestList[index].name}
+              <button
+                type="button"
+                onClick={() => removeHobby(index)}
+                className="removeHobbyBtn"
+              >
+                Remove Hobby
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default RenderInterests;
