0b6e9133022f7c3720b8b1b1686f696f352c198e
Saad Niazi <saadniazifed@gmail.com>
2022-07-17 14:21:58 +0500

    Education component has been refactored with Function Based components

--- src/components/MainContent/Education/Education.js
+++ src/components/MainContent/Education/Education.js
@@ -1,148 +1,115 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import RenderEducation from "./RenderEducation";
 import "./Education.css";
 
-class Education extends Component {
-  constructor(props) {
-    super(props);
+const Education = () => {
+  const [education, setEducation] = useState({
+    educationLevel: "",
+    name: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+  });
 
-    this.state = {
-      education: {
-        educationLevel: "",
-        name: "",
-        startDate: "",
-        endDate: "",
-        description: "",
-      },
+  const [educationList, setEducationList] = useState([]);
+  const [formVisible, setFormVisible] = useState(false);
 
-      educationList: [],
-
-      formVisible: false,
-    };
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      education: {
-        ...this.state.education,
-        [event.target.name]: event.target.value,
-      },
+  const handleChange = (event) => {
+    setEducation({
+      ...education,
+      [event.target.name]: event.target.value,
     });
   };
 
-  addEducation = (e) => {
+  const addEducation = (e) => {
     e.preventDefault();
-    this.setState({
-      ...this.educationList,
-      educationList: this.state.educationList.concat(this.state.education),
-      education: {
-        educationLevel: "",
-        name: "",
-        startDate: "",
-        endDate: "",
-        description: "",
-      },
-    });
+    setEducationList([
+      ...educationList,
+      { educationList: [...educationList, education] },
+    ]);
   };
 
-  removeEducation = (index) => {
-    let newArray = this.state.educationList;
-    let pos = newArray.indexOf(index);
-    newArray.splice(pos, 1);
-    this.setState({
-      educationList: newArray,
-    });
+  const removeEducation = (index) => {
+    setEducationList([
+      ...educationList.slice(0, index),
+      ...educationList.slice(index + 1, educationList.length),
+    ]);
   };
 
-  showForm = () => {
-    this.setState({
-      formVisible: true,
-    });
+  const showForm = () => {
+    setFormVisible(true);
   };
 
-  hideForm = () => {
-    this.setState({
-      formVisible: false,
-    });
+  const hideForm = () => {
+    setFormVisible(false);
   };
 
-  render() {
-    const { educationList } = this.state;
-    return (
-      <>
-        <h3>Education: </h3>
-        <button type="button" onClick={this.showForm} className="projectBtn">
-          Show Form
-        </button>
-        {this.state.formVisible && (
-          <div className="form__group field">
-            <input
-              placeholder="School/College/University"
-              type="text"
-              name="educationLevel"
-              value={this.state.education.educationLevel}
-              onChange={this.handleChange}
-              className="form__field"
-            />
+  return (
+    <>
+      <h3>Education: </h3>
+      <button type="button" onClick={showForm} className="projectBtn">
+        Show Form
+      </button>
+      {formVisible && (
+        <div className="form__group field">
+          <input
+            placeholder="School/College/University"
+            type="text"
+            name="educationLevel"
+            value={education.educationLevel}
+            onChange={handleChange}
+            className="form__field"
+          />
 
-            <input
-              placeholder="Name of University"
-              type="text"
-              name="name"
-              value={this.state.education.name}
-              onChange={this.handleChange}
-              className="form__field"
-            />
+          <input
+            placeholder="Name of University"
+            type="text"
+            name="name"
+            value={education.name}
+            onChange={handleChange}
+            className="form__field"
+          />
 
-            <input
-              type="date"
-              name="startDate"
-              value={this.state.education.startDate}
-              onChange={this.handleChange}
-              className="form__field"
-            />
+          <input
+            type="date"
+            name="startDate"
+            value={education.startDate}
+            onChange={handleChange}
+            className="form__field"
+          />
 
-            <input
-              type="date"
-              name="endDate"
-              value={this.state.education.endDate}
-              onChange={this.handleChange}
-              className="form__field"
-            />
+          <input
+            type="date"
+            name="endDate"
+            value={education.endDate}
+            onChange={handleChange}
+            className="form__field"
+          />
 
-            <textarea
-              value={this.state.education.description}
-              onChange={this.handleChange}
-              name="description"
-              className="textAreaFormField"
-              placeholder="Any personal, academic or other things you have received during your study.."
-            />
-            <div className="projectBtnDiv">
-              <button
-                type="button"
-                onClick={this.addEducation}
-                className="projectBtn"
-              >
-                Add Education
-              </button>
+          <textarea
+            value={education.description}
+            onChange={handleChange}
+            name="description"
+            className="textAreaFormField"
+            placeholder="Any personal, academic or other things you have received during your study.."
+          />
+          <div className="projectBtnDiv">
+            <button type="button" onClick={addEducation} className="projectBtn">
+              Add Education
+            </button>
 
-              <button
-                type="button"
-                onClick={this.hideForm}
-                className="projectBtn"
-              >
-                Hide Form
-              </button>
-            </div>
+            <button type="button" onClick={hideForm} className="projectBtn">
+              Hide Form
+            </button>
           </div>
-        )}
-        <RenderEducation
-          educationList={educationList}
-          removeEducation={this.removeEducation}
-        />
-      </>
-    );
-  }
-}
+        </div>
+      )}
+      <RenderEducation
+        educationList={educationList}
+        removeEducation={removeEducation}
+      />
+    </>
+  );
+};
 
 export default Education;
--- src/components/MainContent/Education/RenderEducation.js
+++ src/components/MainContent/Education/RenderEducation.js
@@ -1,58 +1,52 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from "react";
+import React from "react";
 import uniqid from "uniqid";
 
-class RenderEducation extends Component {
-  constructor(props) {
-    super(props);
-  }
+const RenderEducation = (props) => {
+  const { educationList, removeEducation } = props;
 
-  render() {
-    const { educationList, removeEducation } = this.props;
-
-    return (
-      <>
-        {educationList.map((eduProject, index) => {
-          return (
-            <div key={uniqid()} className="educationDataWrapper">
-              <div className="educationLevel">
-                <h3>Type of Institute: </h3>
-                <p>{eduProject.educationLevel}</p>
-              </div>
-
-              <div className="educationName">
-                <h3>Education Name: </h3>
-                <p>{eduProject.name}</p>
-              </div>
-
-              <div className="educationStartDate">
-                <h3>Start Date: </h3>
-                <p>{eduProject.startDate}</p>
-              </div>
-
-              <div className="educationEndDate">
-                <h3>End Date: </h3>
-                <p>{eduProject.endDate}</p>
-              </div>
-
-              <div className="educationDescription">
-                <h3>Description: </h3>
-                <p>{eduProject.description}</p>
-              </div>
-
-              <button
-                type="button"
-                className="projectBtn"
-                onClick={() => removeEducation(index)}
-              >
-                Remove Education
-              </button>
+  return (
+    <>
+      {educationList.map((eduProject, index) => {
+        return (
+          <div key={uniqid()} className="educationDataWrapper">
+            <div className="educationLevel">
+              <h3>Type of Institute: </h3>
+              <p>{eduProject.educationList[index].educationLevel}</p>
             </div>
-          );
-        })}
-      </>
-    );
-  }
-}
+
+            <div className="educationName">
+              <h3>Education Name: </h3>
+              <p>{eduProject.educationList[index].name}</p>
+            </div>
+
+            <div className="educationStartDate">
+              <h3>Start Date: </h3>
+              <p>{eduProject.educationList[index].startDate}</p>
+            </div>
+
+            <div className="educationEndDate">
+              <h3>End Date: </h3>
+              <p>{eduProject.educationList[index].endDate}</p>
+            </div>
+
+            <div className="educationDescription">
+              <h3>Description: </h3>
+              <p>{eduProject.educationList[index].description}</p>
+            </div>
+
+            <button
+              type="button"
+              className="projectBtn"
+              onClick={() => removeEducation(index)}
+            >
+              Remove Education
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 export default RenderEducation;
