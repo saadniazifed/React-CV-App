2e162d0997d8f6182cb64c1ef1a6d9628a33a624
Saad Niazi <saadniazifed@gmail.com>
2022-07-17 14:28:17 +0500

    Contact component has been refactored using Function Based components.

--- src/components/SidebarComponent/Contact/Contact.js
+++ src/components/SidebarComponent/Contact/Contact.js
@@ -1,55 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 
-class Contact extends Component {
-  constructor(props) {
-    super(props);
+const Contact = () => {
+  const [contact, setContact] = useState({
+    phone: "",
+    email: "",
+    linkedIn: "",
+  });
 
-    this.state = {
-      phone: "",
-      email: "",
-      linkedIn: "",
-    };
-  }
-
-  handleChange = (event) => {
-    this.setState({
+  const handleChange = (event) => {
+    setContact({
+      ...contact,
       [event.target.name]: event.target.value,
     });
   };
 
-  render() {
-    return (
-      <div className="contact--container">
-        <div className="form__group field centerContact">
-          <input
-            name="phone"
-            value={this.state.phone}
-            onChange={this.handleChange}
-            placeholder="+1-234-5678"
-            className="form__field"
-            type="number"
-          />
+  return (
+    <div className="contact--container">
+      <div className="form__group field centerContact">
+        <input
+          name="phone"
+          value={contact.phone}
+          onChange={handleChange}
+          placeholder="+1-234-5678"
+          className="form__field"
+          type="number"
+        />
 
-          <input
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            placeholder="example@example.com"
-            className="form__field"
-          />
+        <input
+          name="email"
+          value={contact.email}
+          onChange={handleChange}
+          placeholder="example@example.com"
+          className="form__field"
+        />
 
-          <input
-            name="linkedIn"
-            value={this.state.linkedIn}
-            onChange={this.handleChange}
-            placeholder="Linkedin.com/abcd"
-            className="form__field"
-          />
-        </div>
+        <input
+          name="linkedIn"
+          value={contact.linkedIn}
+          onChange={handleChange}
+          placeholder="Linkedin.com/abcd"
+          className="form__field"
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Contact;
